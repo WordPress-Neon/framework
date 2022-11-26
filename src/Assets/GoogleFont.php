61a6afadf7d02d34d9264facf8beece845b1744b
Josh <josh@bonnick.dev>
2022-11-26 20:40:42 +0000

    feat(fonts): require only one call to font method when admin is required

--- src/Assets/GoogleFont.php
+++ src/Assets/GoogleFont.php
@@ -7,13 +7,23 @@ use WPN\Support\Stringable;
 
 class GoogleFont {
 	public static function load( string $name, string $file, bool $admin = false ): void {
-		add_action( $admin ? 'admin_enqueue_scripts' : 'wp_enqueue_scripts', function () use ( $file, $name ) {
-			wp_enqueue_style(
-				Stringable::slug( $name ),
-				$file,
-				[],
-				App::environment( 'production' ) ? time() : false
-			);
+		add_action( 'wp_enqueue_scripts', function () use ( $file, $name ) {
+			static::enqueue( $file, $name );
 		} );
+		
+		if ( $admin ) {
+			add_action( 'admin_enqueue_scripts', function () use ( $file, $name ) {
+				static::enqueue( $file, $name );
+			} );
+		}
+	}
+
+	private static function enqueue( $file, $name ): void {
+		wp_enqueue_style(
+			Stringable::slug( $name ),
+			$file,
+			[],
+			App::environment( 'production' ) ? time() : false
+		);
 	}
 }
