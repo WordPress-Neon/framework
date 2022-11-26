57601106c492754632ea8f378772c78491ecdded
Josh <josh@bonnick.dev>
2022-11-26 19:29:15 +0000

    bugs: fix rest api response return

--- src/Support/Api/RestApi.php
+++ src/Support/Api/RestApi.php
@@ -5,24 +5,34 @@ namespace WPN\Support\Api;
 use Closure;
 use ReflectionClass;
 
-class RestApi
-{
-    public static function route(string $route, string $method, string|Closure $callback, string|Closure $middleware)
-    {
-        $callback = is_callable($callback) ? $callback : function () use ($callback) {
-            (new $callback())();
-        };
+class RestApi {
+	public static function route(
+		string $route,
+		string $method,
+		string|Closure $callback,
+		string|Closure $middleware
+	) {
+		$callback = is_callable( $callback ) ? $callback : function ( $request ) use ( $callback ) {
+			return ( new $callback() )( $request );
+		};
 
-        $middleware_callback = is_callable($middleware) ? $middleware : function () use ($middleware) {
-            $reflection = new ReflectionClass($middleware);
+		$middleware_callback = is_callable( $middleware ) ? $middleware : function ( $request ) use ( $middleware ) {
+			$reflection = new ReflectionClass( $middleware );
 
-            return $reflection->hasMethod('authenticate') ? (new $middleware())->authenticate() : (new $middleware())();
-        };
+			return $reflection->hasMethod( 'authenticate' ) ? ( new $middleware() )->authenticate( $request ) : ( new $middleware() )( $request );
+		};
 
-        register_rest_route('theme/v1', $route, array(
-            'methods'             => $method,
-            'callback'            => $callback,
-            'permission_callback' => $middleware_callback,
-        ));
-    }
+		add_action( 'rest_api_init', function () use (
+			$route,
+			$method,
+			$callback,
+			$middleware_callback
+		) {
+			register_rest_route( 'theme/v1', $route, array(
+				'methods'             => $method,
+				'callback'            => $callback,
+				'permission_callback' => $middleware_callback,
+			) );
+		} );
+	}
 }
