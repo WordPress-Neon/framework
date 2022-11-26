42fc5cf9a8652b3d09b9689aaf74c59879963423
Josh <josh@bonnick.dev>
2022-11-26 18:07:51 +0000

    bug: fix rest api middleware

--- src/Support/Api/RestApi.php
+++ src/Support/Api/RestApi.php
@@ -3,17 +3,26 @@
 namespace WPN\Support\Api;
 
 use Closure;
+use ReflectionClass;
 
-class RestApi {
-	public static function route( string $route, string $method, string|Closure $callback, Middleware $middleware ) {
-		$callback = is_callable( $callback ) ? $callback : function () use ( $callback ) {
-			( new $callback() )();
-		};
+class RestApi
+{
+    public static function route(string $route, string $method, string|Closure $callback, string|Closure $middleware)
+    {
+        $callback = is_callable($callback) ? $callback : function () use ($callback) {
+            (new $callback())();
+        };
 
-		register_rest_route( 'theme/v1', $route, array(
-			'methods'             => $method,
-			'callback'            => $callback,
-			'permission_callback' => fn() => ( new $middleware )(),
-		) );
-	}
+        $middleware_callback = is_callable($middleware) ? $middleware : function () use ($middleware) {
+            $reflection = new ReflectionClass($middleware);
+
+            return $reflection->hasMethod('authenticate') ? (new $middleware())->authenticate() : (new $middleware())();
+        };
+
+        register_rest_route('theme/v1', $route, array(
+            'methods'             => $method,
+            'callback'            => $callback,
+            'permission_callback' => $middleware_callback,
+        ));
+    }
 }
