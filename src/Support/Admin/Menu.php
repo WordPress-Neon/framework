96c932eb1db393462e74133cb7fb552a4e93605c
Josh <josh@bonnick.dev>
2022-12-01 15:11:30 +0000

    feat(menus): allow submenu creation chaining

--- src/Support/Admin/Menu.php
+++ src/Support/Admin/Menu.php
@@ -6,7 +6,17 @@ use WPN\Support\Stringable;
 use Closure;
 
 class Menu {
-	public static function register( string $name, string|Closure $callback, int $position = 0, string $icon = 'dashicons-admin-tools' ): string {
+	public function __construct(
+		protected string $name,
+	) {
+	}
+
+	public static function register(
+		string $name,
+		string|Closure $callback,
+		int $position = 0,
+		string $icon = 'dashicons-admin-tools'
+	): self {
 		add_action( 'admin_menu', function () use ( $name, $callback, $icon, $position ) {
 			add_menu_page(
 				$name,
@@ -14,7 +24,7 @@ class Menu {
 				'manage_options',
 				Stringable::slug( $name ),
 				is_callable( $callback ) ? $callback : function () use ( $callback ) {
-					( new $callback )();
+					return ( new $callback )();
 				},
 				$icon,
 				$position
@@ -31,6 +41,12 @@ class Menu {
 			<?php } );
 		}
 
-		return $name;
+		return new self( $name );
+	}
+
+	public function submenu( string $name, Closure|string $callback ): self {
+		Submenu::register( $name, Stringable::slug( $this->name ), $callback );
+
+		return $this;
 	}
 }
