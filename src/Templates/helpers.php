2bfc6b102771e78bcd888e85e8e7ba36f4a4d5c9
Josh <josh@bonnick.dev>
2022-12-01 17:18:44 +0000

    feat(helpers): render and get component variables have same logic

--- src/Templates/helpers.php
+++ src/Templates/helpers.php
@@ -15,6 +15,13 @@ if ( ! function_exists( 'get_component' ) ) {
 
 if ( ! function_exists( 'render_component' ) ) {
 	function render_component( string $path, array|null $variables = null ): void {
-		get_template_part( trailingslashit( app()->templatePartDirectory() ) . $path, null, $variables );
+		$file = get_template_directory() . '/' . trailingslashit( app()->templatePartDirectory() ) . $path . '.php';
+		if ( $variables ) :
+			extract( $variables );
+		endif;
+		ob_start();
+		include $file;
+
+		echo ob_get_clean();
 	}
 }
